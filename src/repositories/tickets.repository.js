f00a6321b53b0b155e7f4ba1ae604b0703f45852
anarehder <anapaula.rehder@gmail.com>
2023-05-29 07:20:28 -0300

    Feat: Join Tickets

--- src/repositories/tickets.repository.js
+++ src/repositories/tickets.repository.js
@@ -1,14 +1,22 @@
 import { db } from "../database/database.connection.js";
 
 export function getTicketsListDB(limit) {
-    if  (limit) {
-    return db.query(`SELECT * FROM tickets WHERE price<$1;`, [limit]);
+    if (limit) {
+        return db.query(`SELECT * FROM tickets WHERE price<$1;`, [limit]);
     }
     return db.query(`SELECT * FROM tickets`);
 }
 
 export function getTicketsByCityDB(city) {
-    return db.query(`SELECT * FROM tickets WHERE "destinyID"=$1;`, [city]);
+    //return db.query(`SELECT * FROM tickets WHERE "destinyID"=$1;`, [city]);
+    return db.query(`SELECT tickets.*, origin.name AS origin, destiny.name AS destiny,
+                        airline.name AS airline, class.name AS class
+                        FROM tickets
+                        JOIN origin ON origin.id = tickets."originID"
+                        JOIN destiny ON destiny.id = tickets."destinyID"
+                        JOIN airline ON airline.id = tickets."airlineID"
+                        JOIN class ON class.id = tickets."classID"
+                        WHERE "destinyID"=$1;`, [city]);
 }
 
 export function getTicketsByIdDB(id) {
